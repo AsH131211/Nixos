1c70e2263133dc16adf78119f2e169c072436019
AsH131211 <aswanth.ash1312@gmail.com>
2026-07-06 20:53:50 +0530

    Almost set 6/7/26

---
+++ modules/graphics.nix
@@ -0,0 +1,21 @@
+{ config, pkgs, ... }:
+
+{
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
+  boot.kernelParams = [ "nvidia-drm.modeset=1" ];
+
+  nixpkgs.config.allowUnfree = true;
+}
